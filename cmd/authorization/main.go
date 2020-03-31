ea41b59c8acf846092d96e061fe56f984077976e
Jesse B. Hannah <jesse@jbhannah.net>
2020-03-31 10:39:42 -0700

    Use Zerolog

--- cmd/authorization/main.go
+++ cmd/authorization/main.go
@@ -6,27 +6,24 @@ import (
 	"crypto/rand"
 	"crypto/x509"
 	"encoding/pem"
-	"fmt"
-	"os"
 
 	authorization "github.com/koverto/authorization/api"
 	"github.com/koverto/authorization/internal/pkg/handler"
 
 	"github.com/koverto/micro/v2"
 	"github.com/micro/go-micro/v2/config/source/env"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
 	pkey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal().AnErr("error", err).Msg("could not generate signing key")
 	}
 
 	enc, err := x509.MarshalECPrivateKey(pkey)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal().AnErr("error", err).Msg("could not read private key")
 	}
 
 	conf := &handler.Config{
@@ -39,23 +36,19 @@ func main() {
 
 	service, err := micro.NewService(authorization.Name, conf, env.NewSource(env.WithStrippedPrefix("KOVERTO")))
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal().AnErr("error", err).Msg("could not initialize service")
 	}
 
 	h, err := handler.New(conf, service)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal().AnErr("error", err).Msg("could not build handler")
 	}
 
 	if err := authorization.RegisterAuthorizationHandler(service.Server(), h); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal().AnErr("error", err).Msg("could not register handler with service")
 	}
 
 	if err := service.Run(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal().AnErr("error", err).Msg("error running service")
 	}
 }
